7625accdecdad7456be38ae49c7a6b9d4d03550d
Amir Muharemovic <muharemovic.amir@gmail.com>
2021-09-16 16:55:12 +0200

    Dodao zadatke za 4 dan

---
+++ ex10/src/index.js
@@ -0,0 +1,18 @@
+function main() {
+    // Only change code below this line
+ var myFirstName, mySecondName, myHomeTown;
+ myFirstName = "Amir";
+ mySecondName = "Muharemović";
+ myHomeTown = "Sarajevo";
+    // Only change code above this line
+
+ return {
+    myFirstName,
+    mySecondName,
+    myHomeTown
+};
+    
+}
+
+console.log(main());
+module.exports=main;
